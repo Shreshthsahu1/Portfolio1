fed70d12bb932eb035bb7f92507b6ebe77719956
Shreshth Sahu <shreshthsahu111@gmail.com>
2026-03-14 19:49:35 +0530

    Fix exhaustive-deps in Scene.tsx for Vercel

--- src/gsap-trial.d.ts
+++ src/gsap-trial.d.ts
@@ -1,9 +1,11 @@
 declare module "gsap-trial/SplitText" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export const SplitText: any;
   export default SplitText;
 }
 
 declare module "gsap-trial/ScrollSmoother" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export const ScrollSmoother: any;
   export default ScrollSmoother;
 }
